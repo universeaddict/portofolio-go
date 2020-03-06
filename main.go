036bb54b879e11bb51fbf4b429016bc37c0c463e
Leonardus Fernando <leociffer@gmail.com>
2020-03-06 23:50:49 +0700

    dependencies

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"portofolio-go/routes"
 	"net/http"
+	"os"
 	"github.com/labstack/echo"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	e := routes.Web()
 	// e.Static("/", "template")
 	e.Static("/", "template")
 	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(port))
 }
 
 // Handler
